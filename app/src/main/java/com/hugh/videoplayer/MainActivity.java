5b4b1d37cfd7bd50ed6c2d5a90202c4e56a92088
MicGun <n1553330708@yahoo.com.tw>
2019-03-29 14:11:53 +0800

    implement video player with surface view and use own controller ui

--- app/src/main/java/com/hugh/videoplayer/MainActivity.java
+++ app/src/main/java/com/hugh/videoplayer/MainActivity.java
@@ -1,19 +1,39 @@
 package com.hugh.videoplayer;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback,
+        MediaPlayer.OnPreparedListener,  VideoControllerView.MediaPlayerControl {
 
     private String mVideoUrl = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4";
+//    private String mVideoUrl = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/protraitVideo.mp4";
 
-    VideoView mVideoView;
+//    VideoView mVideoView;
+//
+//    Button mButton;
+
+    private SurfaceView mSurfaceView;
+    private MediaPlayer mMediaPlayer;
+    private SurfaceHolder mSurfaceHolder;
+    private VideoControllerView mVideoControllerView;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -21,12 +41,166 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mVideoView = findViewById(R.id.videoView);
+        mSurfaceView = findViewById(R.id.surfaceView);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(MainActivity.this);
 
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(mVideoView);
-        mVideoView.setMediaController(mediaController);
-        mVideoView.setVideoPath(mVideoUrl);
-        mVideoView.start();
+        mVideoControllerView = new VideoControllerView(MainActivity.this);
+        mVideoControllerView.show();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setDisplay(mSurfaceHolder);
+
+        try {
+            mMediaPlayer.setDataSource(mVideoUrl);
+            mMediaPlayer.prepare();
+            mMediaPlayer.setOnPreparedListener(MainActivity.this);
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        realeaseMediaPlayer();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realeaseMediaPlayer();
+    }
+
+    private void realeaseMediaPlayer() {
+
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+        mVideoControllerView.setMediaPlayer(this);
+        mVideoControllerView.setAnchorView((FrameLayout) findViewById(R.id.videoSurfaceContainer));
+        mVideoControllerView.show();
+        setVideoSize();
+        mMediaPlayer.start();
+    }
+
+    @Override
+    public void start() {
+        mMediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+
+        mMediaPlayer.pause();
+    }
+
+    @Override
+    public int getDuration() {
+
+        return mMediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+
+        return mMediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int pos) {
+
+        mMediaPlayer.seekTo(pos);
+    }
+
+    @Override
+    public boolean isPlaying() {
+
+        return mMediaPlayer.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isFullScreen() {
+
+        return false;
+    }
+
+    @Override
+    public void toggleFullScreen() {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mVideoControllerView.show();
+        return false;
+    }
+
+    private void setVideoSize() {
+
+        // // Get the dimensions of the video
+        int videoWidth = mMediaPlayer.getVideoWidth();
+        int videoHeight = mMediaPlayer.getVideoHeight();
+        float videoProportion = (float) videoWidth / (float) videoHeight;
+
+        // Get the width of the screen
+        int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+        int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
+        float screenProportion = (float) screenWidth / (float) screenHeight;
+
+        // Get the SurfaceView layout parameters
+        android.view.ViewGroup.LayoutParams lp = mSurfaceView.getLayoutParams();
+        if (videoProportion > screenProportion) {
+            lp.width = screenWidth;
+            lp.height = (int) ((float) screenWidth / videoProportion);
+        } else {
+            lp.width = (int) (videoProportion * (float) screenHeight);
+            lp.height = screenHeight;
+        }
+        // Commit the layout parameters
+        mSurfaceView.setLayoutParams(lp);
     }
 }
